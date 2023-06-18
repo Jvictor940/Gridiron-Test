48c308482ae22ac04f2552efb6308245aabea14f
Jerry Victor <jerryvictor@jerrys-mbp.myfiosgateway.com>
2023-06-17 23:10:41 -0400

    initial route setup

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const dotenv = require('dotenv');
+const athlete = require('./routes/athlete');
+const user = require('./routes/user')
+
 
 dotenv.config({ path: './config/config.env' })
 
@@ -8,6 +11,9 @@ const app = express();
 
 // parse application/json
 app.use(bodyParser.json())
+
+app.use('/athlete', athlete)
+app.use('/user', user)
 
 // Reads from our config.env file || fallback if it can't read our file 
 const PORT = process.env.PORT || 5001
